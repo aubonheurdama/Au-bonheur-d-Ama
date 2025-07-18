593b9ca573a902d06a0a9cc23bcd1f4c3b5f42d9
aubonheurdama <aubonheurdama@gmail.com>
2025-07-18 21:18:08 +0200

    Delete App.js

--- App.js
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <div style={{ padding: '2rem', backgroundColor: '#fff0f5' }}>
-      <h1 style={{ color: '#d63384' }}>Au Bonheur d'Ama</h1>
-      <p>Élevage familial de Border Collie – Chiots disponibles à l'adoption ❤️</p>
-    </div>
-  );
-}
